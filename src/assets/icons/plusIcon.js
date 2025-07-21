71fe3203bb149a79b9c3360cbc6a5a09f29cb57c
gabriellabuenox <gabriellabuenodss@gmail.com>
2025-07-21 17:58:29 -0300

    efeito apoie-nos, ajuste dos azuls

--- src/assets/icons/plusIcon.js
+++ src/assets/icons/plusIcon.js
@@ -8,7 +8,7 @@ const Plus = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <circle cx={18.5} cy={18.5} r={18.5} fill="#002D9F" />
+    <circle cx={18.5} cy={18.5} r={18.5} fill="#2A68D8" />
     <line x1={18.5} y1={9} x2={18.5} y2={30} stroke="white" />
     <line x1={7} y1={19.5} x2={30} y2={19.5} stroke="white" />
   </svg>
