cc7f22aeeb0c4664a7a0aa465e7009665bd126ff
gabriellabuenox <gabriellabuenodss@gmail.com>
2025-07-23 18:35:18 -0300

    todos icons atualizados

--- src/assets/icons/likeIcon.js
+++ src/assets/icons/likeIcon.js
@@ -1,19 +1,20 @@
-import * as React from "react"
-const SvgComponent = (props) => (
+import * as React from "react";
+const SVGComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
+    width={20}
+    height={18}
     fill="none"
+    viewBox="0 0 20 18"
     {...props}
   >
     <path
-      stroke="#000"
+      stroke="#484C52"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={2}
-      d="M12 7.694C10 3 3 3.5 3 9.5s9 11 9 11 9-5 9-11-7-6.5-9-1.806Z"
+      strokeWidth={1.29}
+      d="M10 4.154C8-.54 1-.04 1 5.96s9 11 9 11 9-5 9-11-7-6.5-9-1.806Z"
     />
   </svg>
-)
-export default SvgComponent
+);
+export default SVGComponent;
