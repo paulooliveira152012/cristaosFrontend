29e434196ccb41963bc07d910da878f4bdcb120e
gabriellabuenox <gabriellabuenodss@gmail.com>
2025-07-24 15:14:04 -0300

    ajustes nos comentarios, postagens e footer

--- src/assets/icons/likeIcon.js
+++ src/assets/icons/likeIcon.js
@@ -2,18 +2,18 @@ import * as React from "react";
 const SVGComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={20}
-    height={18}
+    width={21}
+    height={19}
     fill="none"
-    viewBox="0 0 20 18"
+    viewBox="0 0 21 19"
     {...props}
   >
     <path
       stroke="#484C52"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={1.29}
-      d="M10 4.154C8-.54 1-.04 1 5.96s9 11 9 11 9-5 9-11-7-6.5-9-1.806Z"
+      strokeWidth={1.378}
+      d="M10.614 4.37C8.477-.646 1-.112 1 6.297c0 6.41 9.614 11.75 9.614 11.75s9.613-5.34 9.613-11.75-7.477-6.943-9.613-1.929Z"
     />
   </svg>
 );
--- src/assets/icons/likedIcon.js
+++ src/assets/icons/likedIcon.js
@@ -1,10 +1,11 @@
-import * as React from "react"
-const SvgComponent = (props) => (
+import * as React from "react";
+const SVGComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={20}
-    height={18}
+    width={23}
+    height={21}
     fill="none"
+    viewBox="0 0 23 21"
     {...props}
   >
     <path
@@ -12,9 +13,9 @@ const SvgComponent = (props) => (
       stroke="#CE3B3D"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={2}
-      d="M10 4.154C8-.54 1-.04 1 5.96s9 11 9 11 9-5 9-11-7-6.5-9-1.806Z"
+      strokeWidth={2.136}
+      d="M11.614 5.279C9.477.264 2 .799 2 7.207c0 6.41 9.614 11.75 9.614 11.75s9.613-5.34 9.613-11.75c0-6.409-7.477-6.943-9.613-1.928Z"
     />
   </svg>
-)
-export default SvgComponent
+);
+export default SVGComponent;
