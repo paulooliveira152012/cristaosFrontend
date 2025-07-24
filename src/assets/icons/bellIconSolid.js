66a958166f7b1d0b1bfc4266c8a77b39ed175551
gabriellabuenox <gabriellabuenodss@gmail.com>
2025-07-24 15:36:00 -0300

    footer atualizado

---
+++ src/assets/icons/bellIconSolid.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+const BellIconSolid = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <path
+        fill="#222"
+        d="M7.424 21a4.99 4.99 0 0 0 9.152 0H7.424Zm14.968-8.451-1.736-5.723A9.32 9.32 0 0 0 2.58 7.28l-1.348 5.537A5 5 0 0 0 6.09 19h11.517a4.999 4.999 0 0 0 4.785-6.451Z"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default BellIconSolid;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,28 @@
 import { useEffect, useState } from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import "../styles/style.css";
+
 import MenuIcon from "../assets/icons/menuIcon";
 import MessageIcon from "../assets/icons/messageIcon";
-import imagePlaceholder from "../assets/images/profileplaceholder.png";
-import { useUser } from "../context/UserContext"; // Ensure correct import
-import { Link } from "react-router-dom"; // Correct import for Link
-import Plus from "../assets/icons/plusIcon";
+import MessageIconSolid from "../assets/icons/messageIconSolid";
+
 import HomeIcon from "../assets/icons/homeIcon";
+import HomeIconSolid from "../assets/icons/homeIconSolid";
+
+import PlusIcon from "../assets/icons/plusIcon";
+import PlusIconSolid from "../assets/icons/plusIconSolid";
+
 import BellIcon from "../assets/icons/bellIcon";
-import { useNavigate } from "react-router-dom";
+import BellIconSolid from "../assets/icons/bellIconSolid";
+
+import { useUser } from "../context/UserContext";
 import { checkForNewNotifications } from "./functions/footerFunctions";
 
 const Footer = () => {
   const navigate = useNavigate();
-  const { currentUser } = useUser(); // Destructure currentUser from useUser hook
+  const location = useLocation();
+  const { currentUser } = useUser();
   const [notifications, setNotifications] = useState(false);
-  // console.log("Current user in footer component is is:", currentUser)
-  // console.log("currentUser.userId in footer component is:", currentUser._id)
 
   const navigateToMainChat = () => {
     if (currentUser) {
@@ -27,68 +33,37 @@ const Footer = () => {
   };
 
   useEffect(() => {
-    console.log("✅✅✅✅currentUser:", currentUser);
-
-    if (!currentUser) {
-      console.log("no current user...");
-      return;
-    }
-
-    console.log(
-      `checking for new notifications for ${currentUser?.username}...`
-    );
+    if (!currentUser) return;
 
     const timeout = setTimeout(() => {
       checkForNewNotifications(setNotifications);
-    }, 1000); // espera 1 segundo
+    }, 1000);
 
-    return () => clearTimeout(timeout); // boa prática: limpa timeout se componente desmontar
-    
+    return () => clearTimeout(timeout);
   }, [currentUser]);
 
   return (
     <div className="footerContainer">
       <Link to={"/"}>
-        <HomeIcon />
+        {location.pathname === "/" ? <HomeIconSolid /> : <HomeIcon />}
       </Link>
 
-      {/* <MenuIcon /> */}
       <div onClick={navigateToMainChat}>
-        <MessageIcon />
+        {location.pathname === "/chat" ? <MessageIconSolid /> : <MessageIcon />}
       </div>
 
-      {/* conditionally render Plus button */}
       {currentUser && (
         <Link to="/newlisting">
-          <Plus />
+          {location.pathname === "/newlisting" ? <PlusIconSolid /> : <PlusIcon />}
         </Link>
       )}
 
-      {notifications && (
-        <div className="BellIcon">
-          <Link to="/notifications">
-            <BellIcon />
-            <span className="notificationStatus"></span>
-          </Link>
-        </div>
-      )}
-
-      {!notifications && (
-        <div className="BellIcon">
-          <Link to="/notifications">
-            <BellIcon />
-          </Link>
-        </div>
-      )}
-
-      {/* <div
-                className='footerProfileImage'
-                style={{
-                    backgroundImage: `url(${currentUser?.profileImage || imagePlaceholder})`, // Use profile image or fallback image
-                    backgroundPosition: 'center'
-                }}
-                onClick={() => navigate(`profile/${currentUser._id}`)}
-            ></div> */}
+      <div className="BellIcon">
+        <Link to="/notifications">
+          {location.pathname === "/notifications" ? <BellIconSolid /> : <BellIcon />}
+          {notifications && <span className="notificationStatus"></span>}
+        </Link>
+      </div>
     </div>
   );
 };
